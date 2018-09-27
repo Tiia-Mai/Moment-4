5e0745ae83e427e134c65e6e0e3a6e313378136e
Tiia-Mai <tiia.mai.truup@gmail.com>
2018-09-27 15:56:25 +0200

    first commit

---
+++ typescript.js
@@ -0,0 +1,30 @@
+"use strict";
+exports.__esModule = true;
+var fs = require('fs');
+function readContent(callback) {
+    fs.readFile('hitch.txt', 'utf8', function (err, data) {
+        if (err)
+            throw err;
+        callback(null, data);
+    });
+}
+readContent(function (err, data) {
+    var reg = /\n| /; // Strip of all new lines and blanks
+    var clean = data.split(reg);
+    var count = {}; //create array count what includes all words and upcoming amount
+    for (var _i = 0, clean_1 = clean; _i < clean_1.length; _i++) {
+        var i_1 = clean_1[_i];
+        count[i_1] = (count[i_1] || 0) + 1;
+    }
+    var sorted = []; // Declare a new array
+    for (var key in count)
+        sorted.push([key, count[key]]); // Create a new array for use - sorted[word] = count;
+    sorted.sort(function (a, b) { return a[1] - b[1]; }); // Sort the array based on the second element (count)
+    sorted.reverse(); // Reverse the result array
+    var num = 0;
+    var i;
+    for (i = num; i < 10; i++) { //Write out first 10 objects in array sorted
+        console.log(sorted[i]);
+    }
+    ;
+});
---
+++ typescript.ts
@@ -0,0 +1,38 @@
+import { readFile } from 'fs';
+
+declare function require(name:string); // Make it possible to use require in TypeScript
+
+var fs = require('fs');
+
+function readContent(callback){
+  
+     fs.readFile('hitch.txt', 'utf8', function(err, data) {  
+    if (err) throw err; 
+    callback(null,data)
+                 
+});
+}
+
+
+readContent(function(err,data){
+    let reg:RegExp = /\n| /;  // Strip of all new lines and blanks
+    const clean = data.split(reg); 
+    
+    let count = {};  //create array count what includes all words and upcoming amount
+    for(let i of clean){
+        count[i] = (count[i]||0) + 1;
+    }
+
+    var sorted = []; // Declare a new array
+    for (let key in count) sorted.push([key, count[key]]); // Create a new array for use - sorted[word] = count;
+    sorted.sort(function (a, b) { return a[1] - b[1] }); // Sort the array based on the second element (count)
+    sorted.reverse(); // Reverse the result array
+    
+    var num:number=0;
+    var i:number;
+    for (i = num; i < 10; i ++){ //Write out first 10 objects in array sorted
+        console.log(sorted[i]);
+    };
+
+    
+})
